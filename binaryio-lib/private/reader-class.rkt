e4878cfad3b1e70c90c6e55f4f16f8d935e2e8aa
Ryan Culpepper <ryanc@racket-lang.org>
2019-02-23 18:31:25 +0100

    remove #:who method args, add generics for performance testing

--- binaryio-lib/private/reader-class.rkt
+++ binaryio-lib/private/reader-class.rkt
@@ -48,13 +48,13 @@
     ;; the current read position. Internally, limits are stored as
     ;; absolute positions.
 
-    (define/public (get-limit)
+    (define/public-final (get-limit)
       (- limit got))
 
-    (define/public (at-limit/eof?)
+    (define/public-final (at-limit/eof?)
       (or (zero? (get-limit)) (eof-object? (peek-byte in))))
 
-    (define/public (push-limit! rel-limit)
+    (define/public-final (push-limit! rel-limit)
       (define new-limit (+ rel-limit got))
       (unless (<= new-limit limit)
         (error 'push-limit! "new limit is beyond existing limit\n  new: ~s\n  old: ~s"
@@ -62,18 +62,18 @@
       (set! limstack (cons limit limstack))
       (set! limit new-limit))
 
-    (define/public (pop-limit!)
+    (define/public-final (pop-limit!)
       (unless (pair? limstack)
         (error 'pop-limit! "empty limit stack"))
       (set! limit (car limstack))
       (set! limstack (cdr limstack)))
 
-    (define/private (check-read-len len #:who who)
+    (define/private (check-read-len who len)
       (unless (<= (+ got len) limit)
         (long-read who len)))
 
     ;; call-with-limit : (U Nat #f) (-> X) -> X
-    (define/public (call-with-limit rel-limit proc)
+    (define/public-final (call-with-limit rel-limit proc)
       ;; Useful for restoring whole limit stack, eg on exception.
       (push-limit! (or rel-limit (get-limit)))
       (define saved-limstack limstack)
@@ -86,7 +86,7 @@
     ;; ----------------------------------------
     ;; Exhaustion checking
 
-    (define/public (check-exhausted what #:who [who 'check-exhausted])
+    (define/public-final (check-exhausted what #:who [who 'check-exhausted])
       (unless (at-limit/eof?)
         (define-values (line col pos) (port-next-location in))
         (error who "bytes left over~a\n  at: ~a:~a:~a:~a"
@@ -96,33 +96,68 @@
     ;; ----------------------------------------
     ;; Reading w/ exact length required
 
-    (define/public (base-read-bytes! buf start end #:who who)
-      (check-read-len (- end start) #:who who)
+    (define/private (-read-bytes! who buf start end)
+      (check-read-len who (- end start))
       (define r (r:read-bytes! buf in start end))
       (cond [(eof-object? r) 0]
             [else (set! got (+ got r)) r]))
 
-    (define/public (read-bytes! buf [start 0] [end (bytes-length buf)] #:who [who 'read-bytes!])
-      (define r (base-read-bytes! buf start end #:who who))
-      (cond [(< r (- end start)) (short-read who (- end start) buf start (+ start r))]
+    (define/public-final (read-bytes! buf [start 0] [end (bytes-length buf)])
+      (define r (-read-bytes! 'read-bytes! buf start end))
+      (cond [(< r (- end start))
+             (short-read 'read-bytes! (- end start) buf start (+ start r))]
             [else r]))
 
-    (define/public (read-bytes len #:who [who 'read-bytes])
+    (define/private (-read-bytes who len)
       (define buf (make-bytes len))
-      (define r (base-read-bytes! buf 0 len #:who who))
+      (define r (-read-bytes! who buf 0 len))
       (cond [(< r len) (short-read who len buf 0 r)]
             [else buf]))
 
-    (define/public (read-byte #:who [who 'read-byte])
-      (check-read-len 1 #:who who)
+    (define/public-final (read-bytes len)
+      (-read-bytes 'read-bytes len))
+
+    (define/private (-read-byte who)
+      (check-read-len who 1)
       (define b (r:read-byte in))
       (cond [(eof-object? b) (short-read who #"" 0 0)]
             [else (set! got (add1 got)) b]))
 
-    (define/public (read-integer size signed? [big-endian? #t] #:who [who 'read-integer])
-      (bytes->integer (read-bytes size #:who who) signed? big-endian?))
+    (define/public-final (read-byte)
+      (-read-byte 'read-byte))
 
-    (define/public (read-float size [big-endian? #t] #:who [who 'read-float])
-      (floating-point-bytes->real (read-bytes size #:who who) big-endian?))
+    (define/private (-read-integer who size signed? big-endian?)
+      (bytes->integer (-read-bytes who size) signed? big-endian?))
+
+    (define/public-final (read-integer size signed? [big-endian? #t])
+      (-read-integer 'read-integer size signed? big-endian?))
+
+    (define/private (-read-float who size big-endian?)
+      (floating-point-bytes->real (-read-bytes who size) big-endian?))
+
+    (define/public-final (read-float size [big-endian? #t])
+      (-read-float 'read-float size big-endian?))
 
     ))
+
+(define-syntax-rule (define-generics (fun meth (arg ...) ...) ...)
+  (begin
+    (define fun
+      (let ([gen (generic binary-reader% meth)])
+        (case-lambda [(obj arg ...) (send-generic obj gen arg ...)] ...)))
+    ...))
+
+(define-generics
+  (br-get-limit     get-limit     ())
+  (br-at-limit/eof? at-limit/eof? ())
+  (br-push-limit!   push-limit!   (limit))
+  (br-pop-limit!    pop-limit!    ())
+  (br-read-bytes    read-bytes    (len))
+  (br-read-bytes!   read-bytes!   (buf) (buf start end))
+  (br-read-byte     read-byte     ())
+  (br-read-integer  read-integer  (size signed?) (size signed? big-endian?))
+  (br-read-float    read-float    (size signed?))
+  )
+
+(define (make-binary-reader in #:limit [limit +inf.0])
+  (new binary-reader% (in in) (limit limit)))
